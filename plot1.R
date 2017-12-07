587544733591b8d117879090fc9ec7e86cd07463
wied2000 <widya_sagem@yahoo.com>
2017-12-07 15:34:06 +0100

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,11 @@
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip", destfile="w1data.zip")
+unzip ("w1data.zip")
+w1data<-read.csv("household_power_consumption.txt",header=TRUE, na.strings="?",sep=";")
+w1datasub=subset(w1data,w1data$Date=="1/2/2007" | w1data$Date =="2/2/2007")
+
+#Plot1
+hist(as.numeric(w1datasub$Global_active_power), main="Global Active Power", xlab="Global Active Power (kilowatts)", col="Red")
+
+## export file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
